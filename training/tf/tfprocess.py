6cea0c5ff0379f747e503b337b6d6d02c2617f37
godmoves <514158072@qq.com>
2018-04-05 14:48:30 +0800

    debug

--- training/tf/tfprocess.py
+++ training/tf/tfprocess.py
@@ -66,8 +66,8 @@ def optimistic_restore(session, save_file, graph=tf.get_default_graph()):
 class TFProcess:
     def __init__(self):
         # Network structure
-        self.RESIDUAL_FILTERS = 16
-        self.RESIDUAL_BLOCKS = 3
+        self.RESIDUAL_FILTERS = 4
+        self.RESIDUAL_BLOCKS = 2
 
         # Set number of GPUs for training
         self.gpus_num = 2
